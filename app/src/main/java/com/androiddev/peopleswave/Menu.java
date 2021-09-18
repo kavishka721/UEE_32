43ab05aa2c553a443790b6959006ab3a2b8d1a2d
Tharindu Balasooriya <53065333+TharinduBalasooriya@users.noreply.github.com>
2021-09-18 21:20:36 +0530

    Setup

--- app/src/main/java/com/androiddev/peopleswave/Menu.java
+++ app/src/main/java/com/androiddev/peopleswave/Menu.java
@@ -6,20 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
-    /*
-     Navigate to menu page
-    * */
 
-    public void navigateToMenu(View view){
-        Intent intent =  new Intent(this,Menu.class);
+    public void navigateToTransferService(View view){
+        Intent intent = new Intent(this,transferServices.class);
         startActivity(intent);
-
     }
 }
