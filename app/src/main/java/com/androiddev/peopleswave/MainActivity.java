80ea8cd21383974e53f2e5d499ebbb8f15b46e2f
Dinuja-Spinto <sri.prabasha@gmail.com>
2021-09-19 15:57:12 +0530

    AllDone

--- app/src/main/java/com/androiddev/peopleswave/MainActivity.java
+++ app/src/main/java/com/androiddev/peopleswave/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
     /*
      Navigate to menu page
     * */
